9d57e1edd807fa2362881f50e70ff9c10d526f86
jigar.kapadia <jigar.kapadia@learningmate.com>
2020-03-16 15:54:51 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+
+const cron = require("node-cron");
+const express = require("express");
+const fs = require("fs");
+const sendGrid = require('@sendgrid/mail');
+sendGrid.setApiKey('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')
+app = express();
+
+cron.schedule("* * * * *", () => {
+    var currentTime = new Date().toLocaleTimeString('en-US', { hour12: false, hour: "numeric", minute: "numeric"});
+    //console.log(new Date().toLocaleTimeString('en-US', { hour12: false, hour: "numeric", minute: "numeric"}));
+    //console.log("running a task every minute");
+    switch (currentTime) {
+        case '18:59':
+            const msg = {
+                to: 'jigark00@gmail.com',
+                from: 'support@jk.com',
+                subject: 'Sending with SendGrid is Fun',
+                text: 'and easy to do anywhere, even with Node.js',
+                html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+              };
+              sendGrid.send(msg, (err, response) =>{
+                  if(err)
+                  {
+                      console.log('Error')
+                      console.log(err);
+                  }
+                  else
+                  {
+                      console.log(response);
+                  }
+              });
+          break;
+        case 'Mangoes':
+        case 'Papayas':
+          console.log('Mangoes and papayas are $2.79 a pound.');
+          // expected output: "Mangoes and papayas are $2.79 a pound."
+          break;
+        default:
+          console.log('No tasks or reminder available at ' + currentTime + '.');
+      }
+    
+});
+
+app.listen(3001, ()=> {
+    console.log(new Date().toLocaleTimeString('en-US', { hour12: false, hour: "numeric", minute: "numeric"}));
+    console.log(new Date().toLocaleTimeString('en-US', { hour12: false, hour: "numeric", minute: "numeric"}) === '18:49');
+})
+
